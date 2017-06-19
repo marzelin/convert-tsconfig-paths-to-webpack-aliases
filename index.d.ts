879c3622fc61e946a762d6d8ada761f3f06c5f37
marzelin <marzelin@gmail.com>
2017-06-19 14:37:44 +0200

    remove unwanted declaration file from git

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: (tsconfigFile: {
-    baseUrl: string;
-    paths: {
-        [aliasName: string]: string[];
-    };
-}, dirname?: string) => {
-    [aliasName: string]: string;
-};
-export default _default;
